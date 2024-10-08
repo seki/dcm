dfdc38ff8f1e55ab0b2d12c0fc34c2db8b15cdec
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2024-10-08 23:51:54 +0900

    charset

--- lib/dcm/charset.rb
+++ lib/dcm/charset.rb
@@ -7,7 +7,8 @@ module DCM_CharSet
     def initialize(dcm_00080005)
       @charset_names = parse_charset(dcm_00080005)
       ary = @charset_names.map {CharactorSet[_1]}
-      if ary.size == 1 && ary.first[:extension].nil?
+
+      if ary.size == 1 && ary.first[:elements].nil?
         @wo_extensions = true
         @encoding = ary.first[:encoding]
       else
@@ -57,6 +58,7 @@ module DCM_CharSet
 
       result = []
       scan(str).each do |seg|
+        pp element
         if seg[0] == "\e"
           e = @allow_encoding[seg]
           element[e.code_element] = e
@@ -78,19 +80,21 @@ module DCM_CharSet
   end
 end
 
-
 module DCM_CharSet
   class Element
-    def initialize(code_element, escape_sequence, encoding, bytes_per_code_point)
+    def initialize(code_element, escape_sequence, encoding)
       @code_element = code_element
-      @escape_sequence = escape_sequence.pack('C*')
+      @escape_sequence = escape_sequence.pack('c*')
       @encoding = encoding
-      @bytes_per_code_point = bytes_per_code_point
     end
     attr_reader :escape_sequence, :encoding, :code_element
 
     def encode(str)
       str.force_encoding(@encoding)
+    end
+
+    def inspect
+      "#<#{self.class.to_s}:#{@escape_sequence.inspect} #{@encoding}>"
     end
   end
 
@@ -100,7 +104,7 @@ module DCM_CharSet
     end
   end
 
-  AsciiElement = Element.new('GL', [0x1B, 0x28, 0x42], 'ascii', 1)
+  AsciiElement = Element.new('GL', [0x1B, 0x28, 0x42], 'ascii')
 
   CharactorSet = {
     # single-byte w/o extensions
@@ -165,7 +169,7 @@ module DCM_CharSet
       :extension => true,
       :elements => [
         AsciiElement, 
-        Element.new('GR', [0x1B, 0x2D, 0x41], 'ISO_8859_1', 1)
+        Element.new('GR', [0x1B, 0x2D, 0x41], 'ISO_8859_1')
       ]
     },
 
@@ -173,7 +177,7 @@ module DCM_CharSet
       :extension => true,
       :elements => [
         AsciiElement, 
-        Element.new('GR', [0x1B, 0x2D, 0x42], 'iso-8859-2', 1)
+        Element.new('GR', [0x1B, 0x2D, 0x42], 'iso-8859-2')
       ]
     },
 
@@ -181,7 +185,7 @@ module DCM_CharSet
       :extension => true,
       :elements => [
         AsciiElement, 
-        Element.new('GR', [0x1B, 0x2D, 0x43], 'iso-8859-3', 1)
+        Element.new('GR', [0x1B, 0x2D, 0x43], 'iso-8859-3')
       ]
     },
 
@@ -189,7 +193,7 @@ module DCM_CharSet
       :extension => true,
       :elements => [
         AsciiElement, 
-        Element.new('GR', [0x1B, 0x2D, 0x44], 'iso-8859-4', 1)
+        Element.new('GR', [0x1B, 0x2D, 0x44], 'iso-8859-4')
       ]
     },
     
@@ -197,7 +201,7 @@ module DCM_CharSet
       :extension => true,
       :elements => [
         AsciiElement, 
-        Element.new('GR', [0x1B, 0x2D, 0x4C], 'iso-8859-5', 1)
+        Element.new('GR', [0x1B, 0x2D, 0x4C], 'iso-8859-5')
       ]
     },
 
@@ -205,7 +209,7 @@ module DCM_CharSet
       :extension => true,
       :elements => [
         AsciiElement, 
-        Element.new('GR', [0x1B, 0x2D, 0x47], 'iso-8859-6', 1)
+        Element.new('GR', [0x1B, 0x2D, 0x47], 'iso-8859-6')
       ]
     },
 
@@ -213,7 +217,7 @@ module DCM_CharSet
       :extension => true,
       :elements => [
         AsciiElement, 
-        Element.new('GR', [0x1B, 0x2D, 0x46], 'iso-8859-7', 1)
+        Element.new('GR', [0x1B, 0x2D, 0x46], 'iso-8859-7')
       ]
     },
 
@@ -221,7 +225,7 @@ module DCM_CharSet
       :extension => true,
       :elements => [
         AsciiElement, 
-        Element.new('GR', [0x1B, 0x2D, 0x48], 'iso-8859-8', 1)
+        Element.new('GR', [0x1B, 0x2D, 0x48], 'iso-8859-8')
       ]
     },
 
@@ -229,7 +233,7 @@ module DCM_CharSet
       :extension => true,
       :elements => [
         AsciiElement, 
-        Element.new('GR', [0x1B, 0x2D, 0x4D], 'iso-8859-9', 1)
+        Element.new('GR', [0x1B, 0x2D, 0x4D], 'iso-8859-9')
       ]
     },
 
@@ -237,8 +241,8 @@ module DCM_CharSet
     'ISO 2022 IR 13' => {
       :extension => true,
       :elements => [
-        Element.new('GL', [0x1B, 0x28, 0x4A], 'cp50221', 1),
-        Element.new('GR', [0x1B, 0x29, 0x49], 'cp50221', 1)
+        Element.new('GL', [0x1B, 0x28, 0x4A], 'cp50221'),
+        Element.new('GR', [0x1B, 0x29, 0x49], 'cp50221')
       ]
     },
 
@@ -246,7 +250,7 @@ module DCM_CharSet
       :extension => true,
       :elements => [
         AsciiElement, 
-        Element.new('GR', [0x1B, 0x2D, 0x54], 'tis-620', 1)
+        Element.new('GR', [0x1B, 0x2D, 0x54], 'tis-620')
       ]
     },
 
@@ -256,8 +260,7 @@ module DCM_CharSet
       :extension => true,
       :multi_byte => true,
       :elements => [
-        Element.new('GL', [0x1B, 0x24, 0x42],
-                    'euc-jp', 2).extend(E_shift_GL_to_GR)
+        Element.new('GL', [0x1B, 0x24, 0x42], 'euc-jp').extend(E_shift_GL_to_GR)
       ]
     },
 
@@ -265,8 +268,7 @@ module DCM_CharSet
       :extension => true,
       :multi_byte => true,
       :elements => [
-        Element.new('GL', [0x1B, 0x24, 0x28, 0x44],
-                    'euc-jp', 2).extend(E_shift_GL_to_GR)
+        Element.new('GL', [0x1B, 0x24, 0x28, 0x44], 'euc-jp').extend(E_shift_GL_to_GR)
       ]
     },
 
@@ -274,7 +276,7 @@ module DCM_CharSet
       :extension => true,
       :multi_byte => true,
       :elements => [
-        Element.new('GR', [0x1B, 0x24, 0x29, 0x43], 'euc-kr', 2)
+        Element.new('GR', [0x1B, 0x24, 0x29, 0x43], 'euc-kr')
       ]
     },
 
@@ -282,7 +284,7 @@ module DCM_CharSet
       :extension => true,
       :multi_byte => true,
       :elements => [
-        Element.new('GR', [0x1B, 0x24, 0x29, 0x41], 'gb18030', 2)
+        Element.new('GR', [0x1B, 0x24, 0x29, 0x41], 'gb18030')
       ]
     },
 
@@ -306,6 +308,11 @@ module DCM_CharSet
 end
 
 if __FILE__ == $0
+
+  def do_it(dcm_00080005, str)
+    pp DCM_CharSet::Context.new(dcm_00080005).convert(str).map {|x| x.encode('utf-8')}
+  end
+
   c = DCM_CharSet::Context.new("\\ISO 2022 IR 87\\ISO 2022 IR 13")
 
   data = [
@@ -319,4 +326,10 @@ if __FILE__ == $0
   ]
   data.each {|s| pp c.convert(s).map {|x| x.encode('utf-8')}}
 
+  do_it("\\ISO 2022 IR 87", "Yamada^Tarou=\033$B;3ED\033(B^\033$BB@O:\033(B=\033$B$d$^$@\033(B^\033$B$?$m$&\033(B")
+  do_it("ISO 2022 IR 13\\ISO 2022 IR 87", "\324\317\300\336^\300\333\263=\033$B;3ED\033(J^\033$BB@O:\033(J=\033$B$d$^$@\033(J^\033$B$?$m$&\033(J")
+  do_it("\\ISO 2022 IR 149", "Hong^Gildong=\033$)C\373\363^\033$)C\321\316\324\327=\033$)C\310\253^\033$)C\261\346\265\277")
+  do_it("ISO_IR 192", "\x57\x61\x6e\x67\x5e\x58\x69\x61\x6f\x44\x6f\x6e\x67\x3d\xe7\x8e\x8b\x5e\xe5\xb0\x8f\xe6\x9d\xb1\x3d")
+  do_it("GB18030", "\x57\x61\x6e\x67\x5e\x58\x69\x61\x6f\x44\x6f\x6e\x67\x3d\xcd\xf5\x5e\xd0\xa1\xb6\xab\x3d")
+  do_it("\\ISO 2022 IR 58", "\x5A\x68\x61\x6E\x67\x5E\x58\x69\x61\x6F\x44\x6F\x6E\x67\x3D\x1B\x24\x29\x41\xD5\xC5\x5E\x1B\x24\x29\x41\xD0\xA1\xB6\xAB\x3D\x20")
 end
